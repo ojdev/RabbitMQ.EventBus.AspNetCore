815f20b7bae0d01d759dd51e532819e26ce442e2
欧俊 <luacloud@gmail.com>
2018-11-14 15:34:05 +0800

    version build

--- src/RabbitMQ.EventBus.AspNetCore/Extensions/TypeExtensions.cs
+++ src/RabbitMQ.EventBus.AspNetCore/Extensions/TypeExtensions.cs
@@ -4,12 +4,26 @@ using System.Linq;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class TypeExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public static IEnumerable<Type> GetAssemblies(this Type type)
         {
             return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes().Where(t => t.GetInterfaces().Contains(type)));
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="interfalceType"></param>
+        /// <param name="makeType"></param>
+        /// <returns></returns>
         public static IEnumerable<Type> GetMakeGenericType(this Type interfalceType, Type makeType)
         {
             return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes().Where(t => t.GetInterfaces().Contains(interfalceType.MakeGenericType(makeType))));
