27df7f75f208501171ed8a4b0f3d5af2c77d55fd
欧俊 <luacloud@gmail.com>
2018-11-14 15:49:46 +0800

    butterfly已停止维护

--- src/modules/RabbitMQ.EventBus.AspNetCore.Butterfly/RabbitMQEventBusModuleOptionExtensions.cs
+++ src/modules/RabbitMQ.EventBus.AspNetCore.Butterfly/RabbitMQEventBusModuleOptionExtensions.cs
@@ -5,6 +5,12 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class RabbitMQEventBusModuleOptionExtensions
     {
+        /// <summary>
+        /// Butterfly已停止维护
+        /// </summary>
+        /// <param name="build"></param>
+        /// <param name="tracer"></param>
+        /// <returns></returns>
         public static RabbitMQEventBusModuleOption AddButterfly(this RabbitMQEventBusModuleOption build, IServiceTracer tracer)
         {
             build.AddModule(ButterflyModuleHandler.Handle(tracer));
