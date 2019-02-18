8699c246c7526b04d938a027351e0cc1672073bd
欧俊 <luacloud@gmail.com>
2019-02-18 09:45:16 +0800

    CodeFactor

---
+++ RabbitMQ.EventBus.AspNetCore.Simple/Controllers/MessageBody.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.EventBus.AspNetCore.Attributes;
+using RabbitMQ.EventBus.AspNetCore.Events;
+using System;
+
+namespace RabbitMQ.EventBus.AspNetCore.Simple.Controllers
+{
+    [EventBus(Exchange = "RabbitMQ.EventBus.Simple", RoutingKey = "rabbitmq.eventbus.test")]
+    public class MessageBody : IEvent
+    {
+        public string Body { get; set; }
+        public DateTimeOffset Time { get; set; }
+    }
+}
--- RabbitMQ.EventBus.AspNetCore.Simple/Controllers/ValuesController.cs
+++ RabbitMQ.EventBus.AspNetCore.Simple/Controllers/ValuesController.cs
@@ -1,52 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using RabbitMQ.EventBus.AspNetCore.Attributes;
-using RabbitMQ.EventBus.AspNetCore.Events;
 using System;
-using System.Threading.Tasks;
 
 namespace RabbitMQ.EventBus.AspNetCore.Simple.Controllers
 {
-    [EventBus(Exchange = "RabbitMQ.EventBus.Simple", RoutingKey = "rabbitmq.eventbus.test")]
-    public class MessageBody : IEvent
-    {
-        public string Body { get; set; }
-        public DateTimeOffset Time { get; set; }
-    }
-    //[EventBus(Exchange = "RabbitMQ.EventBus.Simple", RoutingKey = "rabbitmq.eventbus.test")]
-    [EventBus(Exchange = "RabbitMQ.EventBus.Simple", RoutingKey = "rabbitmq.eventbus.test1")]
-    public class MessageBody1 : IEvent
-    {
-        public string Body { get; set; }
-        public DateTimeOffset Time { get; set; }
-    }
-    public class MessageBodyHandle : IEventHandler<MessageBody1>, IDisposable
-    {
-        private Guid id;
-        private readonly ILogger<MessageBodyHandle> _logger;
-
-        public MessageBodyHandle(ILogger<MessageBodyHandle> logger)
-        {
-            id = Guid.NewGuid();
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
-        public void Dispose()
-        {
-            Console.WriteLine("释放");
-        }
-
-
-        public Task Handle(MessageBody1 message, EventHandlerArgs args)
-        {
-            Console.WriteLine("==================================================");
-            Console.WriteLine(id + "=>" + typeof(MessageBody1).Name);
-            Console.WriteLine(message.Serialize());
-            Console.WriteLine(args.Original);
-            Console.WriteLine(args.Redelivered);
-            Console.WriteLine("==================================================");
-            return Task.CompletedTask;
-        }
-    }
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
