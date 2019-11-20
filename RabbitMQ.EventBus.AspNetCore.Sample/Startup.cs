7701ea61f8385518009b111825207bde5faefd8b
oujun <me@oujun.work>
2019-11-20 17:20:48 +0800

    修改错误的拼写，修改部分类的显示范围

--- RabbitMQ.EventBus.AspNetCore.Sample/Startup.cs
+++ RabbitMQ.EventBus.AspNetCore.Sample/Startup.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Reflection;
 
@@ -36,7 +36,7 @@ namespace RabbitMQ.EventBus.AspNetCore.Simple
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
